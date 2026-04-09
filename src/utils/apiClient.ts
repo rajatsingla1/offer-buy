b37a7b6f2f872d85eb589fa0e893e4a3ca9bc452
Rajat Singla <r31962871@gmail.com>
2026-04-09 11:59:57 +0530

    asd

--- src/utils/apiClient.ts
+++ src/utils/apiClient.ts
@@ -6,8 +6,8 @@ const instance = (baseURL: string) =>
   });
 
 const client = {
-  // root: instance("https://credits-api.alliedoffsets.com"),
-  root: instance("http://localhost:8000"),
+  root: instance("https://credits-api.alliedoffsets.com"),
+  // root: instance("http://localhost:8000"),
 };
 
 export default client;
